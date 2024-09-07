e786c0d771bfa81f53bb7f18a7454255bb729e30
Anastasiia Tykha <anastasiia.tykha@academysmart.com>
2024-09-07 20:20:34 +0300

    added button component and reset css

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/adverts/">
+    <BrowserRouter basename="/foodies-fe">
       <Provider store={store}>
         <App />
       </Provider>
