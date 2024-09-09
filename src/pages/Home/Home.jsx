8b63e07eea27a920fe70bdb53aaafb6b46c99254
Kostiantyn Baltakov <tira2500@gmail.com>
2024-09-09 22:15:40 +0300

    Testimonials initial

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import css from './Home.module.css';
+import { Testimonials } from '../../components/Testimonials/Testimonials.jsx';
 
 export const Home = () => (
   <>
     <section className={css.hero}>HOME</section>
+    <Testimonials />
   </>
 );
